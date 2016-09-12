b887d544df58a42a334a47875ee1b955b88f6a22
Justin Luna <4a4c@users.noreply.github.com>
2016-09-11 20:38:36 -0600

    some docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,5 +1,5 @@
 # Start of some docker aliases
 
-alias vaquero-validate='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedrepos-docker-vaquero.bintray.io/vaquero/vaquero:0.1 validate --sot-dir'
-alias go-build='docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 go build -v'
-alias go-make='docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 bash -c make'
+alias vaquero-validate='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) jl/vaquero:1.0 validate --sot-dir'
+alias go-build='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 go build -v'
+alias go-make='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 bash -c make'
