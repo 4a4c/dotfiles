37598a41db77770be9337cf33b48843e14b3f846
Justin Luna <4a4c@users.noreply.github.com>
2016-09-18 09:57:35 -0600

    pahtogen config

--- vim/install.sh
+++ vim/install.sh
@@ -2,5 +2,14 @@
 
 set -e
 
+# install pathogen
 mkdir -p $DOTFILES/vim/autoload $DOTFILES/vim/bundle && \
-curl -LSso $DOTFILES/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [ ! -f $DOTFILES/vim/autoload/pathogen.vim ]; then
+  curl -LSso $DOTFILES/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+# install sensible-vim
+BUNDLE_DIR='vim-sensible'
+if [ ! -d $DOTFILES/vim/bundle/$BUNDLE_DIR ]; then
+  git clone git://github.com/tpope/vim-sensible.git $DOTFILES/vim/bundle/$BUNDLE_DIR
+fi
