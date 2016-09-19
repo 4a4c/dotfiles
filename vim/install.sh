493cd22049ac9043850cb703d3f5092e5ac87dd1
Justin Luna <4a4c@users.noreply.github.com>
2016-09-18 19:23:46 -0600

    fixing vim/install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -3,20 +3,23 @@
 set -e
 
 # pre-work
+# dirname command is using script dir regardless of where it is executed from
 VIM_DIR=$(dirname -- "$0")
 BUNDLE_DIR="$VIM_DIR/bundle"
 mkdir -p $BUNDLE_DIR
 
-# install pathogen
-BUNDLE_NAME=vim-pathogen
-BUNDLE_REPO="https://github.com/tpope/vim-pathogen.git"
-if [ ! -d  $BUNDLE_DIR/$BUNDLE_NAME ]; then
-	git submodule add $BUNDLE_REPO $BUNDLE_DIR/$BUNDLE_NAME
-fi
+# loop through csv and install bundles
+# tail -n +2 reads all lines starting on 2 (excluders headers)
+tail -n +2 $VIM_DIR/bundles.csv | while read line; do
+	# set some vars
+	CURRENT_LINE=$(echo $line)
+	BUNDLE_NAME=$(echo $CURRENT_LINE | awk -F ',' '{print $1}')
+	BUNDLE_REPO=$(echo $CURRENT_LINE | awk -F ',' '{print $2}')
+	
+	# echo message
+	echo "downloading $BUNDLE_NAME from $BUNDLE_REPO"
 
-BUNDLE_NAME=vim-sensible
-BUNDLE_REPO="https://github.com/tpope/vim-sensible.git"
-if [ ! -d  $BUNDLE_DIR/$BUNDLE_NAME ]; then
-	git submodule add $BUNDLE_REPO $BUNDLE_DIR/$BUNDLE_NAME
-fi
-
+	if [ ! -d  $BUNDLE_DIR/$BUNDLE_NAME ]; then
+		git submodule add $BUNDLE_REPO $BUNDLE_DIR/$BUNDLE_NAME
+	fi
+done
