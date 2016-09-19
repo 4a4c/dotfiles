babc02b10bc269e369a05288c0fb45143af58b8b
Justin Luna <4a4c@users.noreply.github.com>
2016-09-18 18:41:18 -0600

    pathogen and sensible

--- vim/install.sh
+++ vim/install.sh
@@ -2,14 +2,20 @@
 
 set -e
 
+# pre-work
+VIM_DIR=$(dirname -- "$0")
+BUNDLE_DIR="$VIM_DIR/bundle"
+mkdir -p $BUNDLE_DIR
+
 # install pathogen
-mkdir -p $DOTFILES/vim/autoload $DOTFILES/vim/bundle && \
-if [ ! -f $DOTFILES/vim/autoload/pathogen.vim ]; then
-  curl -LSso $DOTFILES/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+BUNDLE_NAME=vim-pathogen
+BUNDLE_REPO="https://github.com/tpope/vim-pathogen.git"
+if [ ! -d  $BUNDLE_DIR/$BUNDLE_NAME ]; then
+	git submodule add $BUNDLE_REPO $BUNDLE_DIR/$BUNDLE_NAME
 fi
 
 # install sensible-vim
-BUNDLE_DIR='vim-sensible'
-if [ ! -d $DOTFILES/vim/bundle/$BUNDLE_DIR ]; then
-  git clone git://github.com/tpope/vim-sensible.git $DOTFILES/vim/bundle/$BUNDLE_DIR
-fi
+#BUNDLE_DIR='vim-sensible'
+#if [ ! -d $DOTFILES/vim/bundle/$BUNDLE_DIR ]; then
+#  git clone git://github.com/tpope/vim-sensible.git $DOTFILES/vim/bundle/$BUNDLE_DIR
+#fi
