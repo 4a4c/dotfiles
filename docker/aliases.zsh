13d78d783149b4db6984a29f9394e377d2585ab7
Justin Luna <4a4c@users.noreply.github.com>
2017-03-20 16:23:52 -0600

    docker commands

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,6 +1,8 @@
 # Start of some docker aliases
 
 alias vaquero-validate='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedrepos-docker-vaquero.bintray.io/vaquero/vaquero:v0.7.0 validate --sot'
+alias vaquero='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedrepos-docker-vaquero.bintray.io/vaquero/vaquero``'
 alias go-build='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 go build -v'
 alias go-make='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 bash -c make'
 alias geronimo='docker run -ti --rm -v $PWD:/configs registry.vipertv.net/viper/geronimo:2.0'
+alias docker-mac='docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh'
