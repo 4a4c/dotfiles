ffc944bed754a021d984b3d0569106e4563b0c1f
Justin Luna <4a4c@users.noreply.github.com>
2017-08-17 09:25:13 -0400

    some stuff

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,5 +4,5 @@ alias vaquero-validate='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedre
 alias vaquero='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedrepos-docker-vaquero.bintray.io/vaquero/vaquero``'
 alias go-build='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 go build -v'
 alias go-make='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 bash -c make'
-alias geronimo='docker run -ti --rm -v $PWD:/configs registry.vipertv.net/viper/geronimo:2.0'
+alias geronimo='docker run -ti --rm -v $PWD:/configs registry.vipertv.net/sde/geronimo:3.0.37'
 alias docker-mac='docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh'
