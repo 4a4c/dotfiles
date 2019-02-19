c705a1c40dd2d45dc0bfbb7f1c96fe36cc4ef424
Justin Luna <4a4c@users.noreply.github.com>
2019-02-19 09:26:30 -0700

    vaq update

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,7 @@
 # Start of some docker aliases
 
 alias vaquero-validate='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedrepos-docker-vaquero.bintray.io/vaquero/vaquero:v0.7.0 validate --sot'
-alias vaquero='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) shippedrepos-docker-vaquero.bintray.io/vaquero/vaquero``'
+alias vaquero='docker run --rm -it -v $(pwd):$(pwd) -w $(pwd) registry.vipertv.net/viper-sde/vaquero:0.14.7``'
 alias go-build='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 go build -v'
 alias go-make='docker run --rm -v "$PWD":/usr/src/myapp -v "$HOME/go":/go -w /usr/src/myapp golang:1.6 bash -c make'
 alias geronimo='docker run -ti --rm -v $PWD:/configs registry.vipertv.net/sde/geronimo:3.0.37'
